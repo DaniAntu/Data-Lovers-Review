f2e4f0300916095a8e3a1d3acc0c82bb65528fb2
DaniAntu <d.asenjo.c@gmail.com>
2019-01-20 20:02:34 -0300

    Agrega botón ir arriba

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 M.AutoInit();
+$(document).ready(function(){
+      $('.go-up').click(function(){
+            $('body, html').animate({
+                  scrollTop: '0px'
+            });
+      });
+});
 const data = (POKEMON.pokemon);
 const cards = document.getElementById('poke-cards');    
 
@@ -12,11 +19,8 @@ window.onload = showData(data);
 function createCardPoke(dataSent){
       // limpio div
       cards.innerHTML = '';
-      console.log('aquiii entra a card');
       dataSent.forEach(element => {
-            console.log('foor each');
             cards.innerHTML += `
-            
             <div class="target col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
                   <div class="card small">
                         <div class="card-image waves-effect waves-block waves-light center-align">
@@ -41,64 +45,26 @@ function createCardPoke(dataSent){
             `
       });        
 }
-
-// document.getElementById('btn_enter').addEventListener('click',(event) => {
-//     event.preventDefault();
-    
-    
-//     document.getElementById('user').innerHTML = '';
-//     menuContainment.innerHTML = `
-//     <article>
-//         <section id="containmentPokes" class="containment containment-pokes">
-//         <!--Aquí va el menú de la página de Pokes-->
-//             <div id = "found" class = "search-div">
-//               <label>Nombre o Número</label>
-//               <input type = "text" id = "id-poke-search" placeholder="Ej: 005, Charmander, etc."> 
-//               <button type="submit" value = "search" id="search" class="button button-search"></button>
-//               <p>Usa la busqueda para encontrar POKÉMON</p>
-//             </div>
-//             <div id = "middle" class="middle-o">
-//                 <span><h5>O</h5></span>
-//             </div> 
-//             </div>
-//             <div class"order">
-//                 <div class="container container-order">
-//                     <select id="order" class = "order">
-//                         <option value="0">Ordena pokemones por...</option> 
-//                         <option value="upward">A - Z</option> 
-//                         <option value="falling">Z - A</option>
-//                         <option value="less">Menor núm ID (Primero)</option>
-//                         <option value="major">Mayor núm ID (Primero) </option>
-//                     </select>
-//                 </div>
-//             <div/>
-//             </div>
-//         </section>
-//             <div id = "box-filter-order">
-//                 <div id = "filter" class = "container container-span" >
-//                     <span class="span-filter" id="Bug"> Insecto </span>
-//                     <span class="span-filter" id="Dragon"> Dragon </span>
-//                     <span class="span-filter" id="Electric"> Electrico </span>
-//                     <span class="span-filter" id="Fighting"> Peleador </span>
-//                     <span class="span-filter" id="Fire"> Fuego </span>
-//                     <span class="span-filter" id="Flying"> Volador </span>
-//                     <span class="span-filter" id="Ghost"> Fantasma </span>
-//                     <span class="span-filter" id="Grass"> Hierba </span>
-//                     <span class="span-filter" id="Ground"> Tierra </span>
-//                     <span class="span-filter" id="Ice"> Hielo </span>
-//                     <span class="span-filter" id="Normal">Normal </span>
-//                     <span class="span-filter" id="Poison"> Veneno </span>
-//                     <span class="span-filter" id="Psychic"> Psíquico </span>
-//                     <span class="span-filter" id="Rock"> Roca </span>
-//                     <span class="span-filter" id = "Water"> Agua </span>
-//                     <span class="span-filter" id = "All"> Todos </span>
-//                 </div>
-//                 <div>
-//                     <span id = "calc"></span>
-//                 </div
-//             </div>
-//         </article>
-//       `
+      //Filtrado
+      spanValueFilter = Array.from(document.getElementsByClassName('span-filter'))
+      spanValueFilter.forEach(function(element){
+            element.addEventListener('click',(event) => {
+                  event.preventDefault();
+                  let valueSpan = element.id;
+                  if (valueSpan === 'All'){
+                  document.getElementById('calc').innerHTML = '';
+                  createCardPoke(data);
+                  } else {
+                  let valueSpain = element.innerHTML;
+                  console.log(valueSpain);
+                  cards.innerHTML = '';
+                  let calculation = window.dataPokemon.probability(data,valueSpan).toFixed(3);
+                  document.getElementById('calc').innerHTML = 'Tienes un ' + calculation + '% de problabilidad de encontrar un pokemón, tipo '+ valueSpain;
+                  let dataFilter = window.dataPokemon.filterPokeType(data,valueSpan);
+                  createCardPoke(dataFilter);
+                  }
+            });
+      });
 
     //Buscar por nombre e ID    
     document.getElementById('search').addEventListener('click',(event) => {
@@ -144,26 +110,6 @@ function createCardPoke(dataSent){
             `
             document.getElementById('id-search').value ="";
         }
-    });
-
-    //Filtrado
-    spanValueFilter = Array.from(document.getElementsByClassName('span-filter'))
-    spanValueFilter.forEach(function(element){
-        element.addEventListener('click',(event) => {
-            event.preventDefault();
-            let valueSpan = element.id;
-            if (valueSpan === 'All'){
-                document.getElementById('calc').innerHTML = '';
-                createCardPoke(data);
-            } else {
-                let valueSpain = element.innerHTML;
-                cards.innerHTML = '';
-                let calculation = probability(data,valueSpan).toFixed(3);
-                document.getElementById('calc').innerHTML = 'Tienes un ' + calculation + '% de problabilidad de encontrar un pokemón, tipo '+ valueSpain;
-                let dataFilter = filterPokeType(data,valueSpan);
-                createCardPoke(dataFilter);
-            }
-        });
     });
     
     //Orden por nombre
